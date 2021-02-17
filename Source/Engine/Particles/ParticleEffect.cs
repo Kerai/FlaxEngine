c2039fd94f7b1378b5316753515da836c071ec43
Keray <keray@keray.eu>
2021-02-17 06:55:19 +0100

    Remove C#

--- Source/Engine/Particles/ParticleEffect.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2012-2021 Wojciech Figat. All rights reserved.
-
-namespace FlaxEngine
-{
-    partial class ParticleEffect
-    {
-        private bool IsFixedTimestep => UpdateMode == SimulationUpdateMode.FixedTimestep;
-    }
-}
